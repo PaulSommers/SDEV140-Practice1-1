e87c787acdf676c9f1adc4546ed6b140fed96b9d
PaulSommers <p.eliotsommers@gmail.com>
2024-10-25 11:24:00 -0700

    Practice1-1

---
+++ myinfo.py
@@ -0,0 +1,16 @@
+"""
+Author: Paul Sommers
+Date written: 10/25/2024
+Assignment: Exercise 1-1
+Short Desc: This program defines variables for my name, address, and phone number, assigns values to them, and outputs the values.
+"""
+
+# Define variables and assign values
+strName = "Paul Sommers"
+strAddress = "5273 Brandywine Drive, Whitestown, IN 46075"
+strPhone = "317-970-6735"
+
+# Display the values of the variables
+print("Name:", strName)
+print("Address:", strAddress)
+print("Phone:", strPhone)
